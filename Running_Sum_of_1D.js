71f7a97c126901da84f5668ad43c2415eff9de48
Usman Ahmad <88334315+6ix9ineJJ@users.noreply.github.com>
2023-08-09 22:40:24 +0500

    Update Running_Sum_of_1D.js

--- Running_Sum_of_1D.js
+++ Running_Sum_of_1D.js
@@ -1,3 +1,7 @@
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+------------------------------------------------------------------------------------------------
+
 var runningSum = function(nums) {
     for(i=1; i<nums.length; i++){
         nums[i]=nums[i]+nums[i-1];
