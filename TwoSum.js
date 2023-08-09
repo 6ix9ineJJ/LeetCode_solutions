363efbb5739419ef88ff0dea6ab4939a6f076ef8
Usman Ahmad <88334315+6ix9ineJJ@users.noreply.github.com>
2023-08-09 22:22:53 +0500

    Add files via upload

---
+++ TwoSum.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    
+    const indexes =[];
+
+    for(let i=0; i<nums.length-1; i++){
+      for(let j=i+1; j<nums.length; j++){
+       if(nums[i]+nums[j]===target){
+         return[i,j]
+       }
+      }
+      }  
+    }
